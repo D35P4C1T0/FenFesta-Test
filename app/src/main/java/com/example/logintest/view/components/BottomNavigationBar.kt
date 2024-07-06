a1835b4c0aa7eccab8771964360744693fda5bad
Matteo <girardi.matteo.dev@gmail.com>
2024-07-06 17:58:22 +0200

    Made navbar buttons the same as primary theme color

--- app/src/main/java/com/example/logintest/view/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/logintest/view/components/BottomNavigationBar.kt
@@ -9,16 +9,21 @@ import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
+import androidx.navigation.Navigator
 import com.example.logintest.view.ScreenCalendar
 import com.example.logintest.view.ScreenMap
 
@@ -55,29 +60,26 @@ fun BottomNavigationBar(navController: NavController) {
         mutableIntStateOf(0)
     }
 
-    NavigationBar {
+    NavigationBar(
+        contentColor = MaterialTheme.colorScheme.primary,
+//        containerColor = MaterialTheme.colorScheme.primary,
+    ) {
         items.forEachIndexed { index, item ->
             NavigationBarItem(
                 icon = {
-                    BadgedBox(
-                        badge = {
-                            if (item.badgeCount != null) {
-                                Badge {
-                                    Text(text = item.badgeCount.toString())
-                                }
-                            } else if (item.hasNews) {
-                                Badge()
-                            }
-                        }
-                    ) {
                         Icon(
                             imageVector = if (index == selectedItemIndex) {
                                 item.selectedIcon
                             } else item.unselectedIcon,
-                            contentDescription = item.title
+                            contentDescription = item.title,
+//                            tint = MaterialTheme.colorScheme.primary
                         )
-                    }
                 },
+                colors = NavigationBarItemDefaults.colors(
+                    unselectedIconColor = Color.Gray,
+                    selectedIconColor = Color.White,
+                    indicatorColor = MaterialTheme.colorScheme.primary
+                ),
                 label = {
                     Text(item.title)
                 },
